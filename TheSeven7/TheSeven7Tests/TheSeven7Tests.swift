6d53114da7a475a525ab5ac5bdd83560306e8e8c
pappasn <pappasn@purdue.edu>
2019-09-20 18:37:54 -0500

    Adding Xcode Project

---
+++ TheSeven7/TheSeven7Tests/TheSeven7Tests.swift
@@ -0,0 +1,34 @@
+//
+//  TheSeven7Tests.swift
+//  TheSeven7Tests
+//
+//  Created by Nick Pappas on 9/20/19.
+//  Copyright © 2019 Nick Pappas. All rights reserved.
+//
+
+import XCTest
+@testable import TheSeven7
+
+class TheSeven7Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
