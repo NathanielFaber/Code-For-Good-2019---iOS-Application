4d7e2b2222c7917f3d3b2654b3d948d46a8d081a
pappasn <pappasn@purdue.edu>
2019-09-20 21:42:23 -0500

    Added password field and character limit

--- TheSeven7/TheSeven7/ParentPasswordViewController.swift
+++ TheSeven7/TheSeven7/ParentPasswordViewController.swift
@@ -7,13 +7,26 @@
 //
 
 import UIKit
+import TextFieldEffects
 
-class ParentPasswordViewController: UIViewController {
+class ParentPasswordViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var passwordTextField: HoshiTextField!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        passwordTextField.delegate = self
+        self.passwordTextField.autocapitalizationType = .allCharacters
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let currentCharacterCount = textField.text?.count ?? 0
+        if range.length + range.location > currentCharacterCount {
+            return false
+        }
+        let newLength = currentCharacterCount + string.count - range.length
+        return newLength <= 4
     }
     
 
