742400e664fc06973660d917695b1d688ea16581
pappasn <pappasn@purdue.edu>
2019-09-20 19:49:37 -0500

    Created parent, parent password, and age selection viewcontrollers

---
+++ TheSeven7/TheSeven7/ParentPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ParentPasswordViewController.swift
+//  TheSeven7
+//
+//  Created by Nick Pappas on 9/20/19.
+//  Copyright © 2019 Ayline Villegas . All rights reserved.
+//
+
+import UIKit
+
+class ParentPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
