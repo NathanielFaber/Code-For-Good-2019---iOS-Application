85b51297752f032e5fd453eec2110f1e33c5f309
sravyap9 <sravyapatakota@gmail.com>
2019-09-21 03:23:24 -0500

    stuff

---
+++ TheSeven7/TheSeven7/TwitterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TwitterViewController.swift
+//  TheSeven7
+//
+//  Created by Sravya Patakota on 9/21/19.
+//  Copyright © 2019 Ayline Villegas . All rights reserved.
+//
+
+import UIKit
+
+class TwitterViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string:"https://twitter.com/chicagochildren")
+        let request = URLRequest(url: url!)
+        webView.loadRequest(request)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
