eee569564e5ebc3f4f16386fea83764a0c4877c3
aylinevillegas <aylinev2@illinois.edu>
2019-09-21 10:35:30 -0500

    fixed youtube controller

--- TheSeven7/TheSeven7/YoutubeViewController.swift
+++ TheSeven7/TheSeven7/YoutubeViewController.swift
@@ -1,34 +1,37 @@
 //
-//  YoutubeViewController.swift
-//  
+//  ColoringBookViewController.swift
+//  TheSeven7
 //
-//  Created by Sravya Patakota on 9/21/19.
+//  Created by Ayline Villegas  on 9/21/19.
+//  Copyright © 2019 Ayline Villegas . All rights reserved.
 //
 
 import UIKit
 import WebKit
 
 class YoutubeViewController: UIViewController {
-
+    
     @IBOutlet weak var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string:"https://www.youtube.com/user/thechicagocac")
+        let url = URL(string:"https://gamesbears.com/coloring-book-mobile/coloring-book-mobile.html")
         let request = URLRequest(url: url!)
         webView.loadRequest(request)
-
+        
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
