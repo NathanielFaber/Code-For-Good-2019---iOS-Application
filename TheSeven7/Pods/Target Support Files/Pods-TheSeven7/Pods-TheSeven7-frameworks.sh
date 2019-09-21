c0280d22867b0da473495d3d6e9b670ccc884282
pappasn <pappasn@purdue.edu>
2019-09-20 21:01:48 -0500

    Added TextField Cocoapod and created a new textfield

--- TheSeven7/Pods/Target Support Files/Pods-TheSeven7/Pods-TheSeven7-frameworks.sh
+++ TheSeven7/Pods/Target Support Files/Pods-TheSeven7/Pods-TheSeven7-frameworks.sh
@@ -154,9 +154,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/PMSuperButton/PMSuperButton.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/TextFieldEffects/TextFieldEffects.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/PMSuperButton/PMSuperButton.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/TextFieldEffects/TextFieldEffects.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
